b8c2fd4c786b20363167ecf28ead10886a9cbd76
ArifBabayev05 <arifrb@code.edu.az>
2022-06-17 15:07:53 +0400

    first commit

---
+++ Fiorello/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Data;
+using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fiorello.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+
+            return View(model: products);
+        }
+        
+    }
+}
